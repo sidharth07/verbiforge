7f729124207f4ef19defa5642a94cd80424cdb21
Sidharth Bandewar <sidharthbandewar@Sids-MacBook-Air.local>
2025-08-20 11:55:33 +0530

    Fix SSL/TLS database connection for staging

--- server.js
+++ server.js
@@ -153,7 +153,10 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: {
+        rejectUnauthorized: false,
+        require: true
+    }
 });
 
 // Test the connection
